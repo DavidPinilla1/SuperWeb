89bc66aada797a71a4a24faa52024793164c466f
David Pinilla <metroid_300@hotmail.com>
2018-12-09 15:26:26 +0100

    functions to switch between languages added in the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,43 +7,52 @@ hide=()=>{
     document.getElementById('Contacto').className = 'hide';
     document.getElementById('englishnav').className = 'hide';
     document.getElementById('spanishnav').className = 'hide';
+    document.getElementById('footcontainerEN').className = 'hide';
+    document.getElementById('footcontainerES').className = 'hide';
 }
 showHome=()=>{
     hide();    
     document.getElementById('englishnav').className = 'mostrarnav';
     document.getElementById('HomeEN').className = 'show';
+    document.getElementById('footcontainerEN').className = 'showfooter';
 }
 showPortfolio=()=> {
     hide();
     document.getElementById('englishnav').className = 'mostrarnav';
     document.getElementById('Portfolio').className = 'show';
+    document.getElementById('footcontainerEN').className = 'showfooter';
 }
 
 showContact=()=>{
     hide();    
     document.getElementById('englishnav').className = 'mostrarnav';
     document.getElementById('Contact').className = 'show';
+    document.getElementById('footcontainerEN').className = 'showfooter';
 }
 
 mostrarIndice=()=>{
     hide();    
     document.getElementById('spanishnav').className = 'mostrarnav';
     document.getElementById('Indice').className = 'show';
+    document.getElementById('footcontainerES').className = 'showfooter';
 }
 mostrarPortfolio=()=>{
     hide();    
     document.getElementById('spanishnav').className = 'mostrarnav';
     document.getElementById('PortfolioES').className = 'show';
+    document.getElementById('footcontainerES').className = 'showfooter';
 }
 mostrarContacto=()=>{
     hide();    
     document.getElementById('spanishnav').className = 'mostrarnav';
     document.getElementById('Contacto').className = 'show';
+    document.getElementById('footcontainerES').className = 'showfooter';
 }
 spanish=()=>{
     const displayed=document.querySelector(".show");
     hide();
     document.getElementById('spanishnav').className = 'mostrarnav';
+    document.getElementById('footcontainerES').className = 'showfooter';
     if(displayed.id==="HomeEN")document.getElementById('Indice').className = 'show';
     else if(displayed.id==="Portfolio")document.getElementById('PortfolioES').className = 'show';
     else if(displayed.id==="Contact")document.getElementById('Contacto').className = 'show';
@@ -52,6 +61,7 @@ english=()=>{
     const displayed=document.querySelector(".show");
     hide();
     document.getElementById('englishnav').className = 'mostrarnav';
+    document.getElementById('footcontainerEN').className = 'showfooter';
     if(displayed.id==="Indice")document.getElementById('HomeEN').className = 'show';
     else if(displayed.id==="PortfolioES")document.getElementById('Portfolio').className = 'show';
     else if(displayed.id==="Contacto")document.getElementById('Contact').className = 'show';
